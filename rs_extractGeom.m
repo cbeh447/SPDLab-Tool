7fbbb92692965f1f3a18c6811ce868d53adf4c62
cbeh447 <52840549+cbeh447@users.noreply.github.com>
2019-07-19 11:34:32 +0200

    Add files via upload

---
+++ rs_extractGeom.m
@@ -0,0 +1,16 @@
+function G = rs_extractGeom(VOL_P,VOL1,VOL2,VOL3,VOL4,VOL5,V_liq,V_gas,area_sc,height_sc,Qintr,Qr,Qw)
+    
+    G.VOL_P  = VOL_P;
+    G.VOL1   = VOL1;
+    G.VOL2   = VOL2;
+    G.VOL3   = VOL3;
+    G.VOL4   = VOL4;
+    G.VOL5   = VOL5;
+    G.V_liq  = V_liq;
+    G.V_gas  = V_gas;
+    G.area_sc   = area_sc;
+    G.height_sc = height_sc;
+    G.Qintr  = Qintr;
+    G.Qr     = Qr;
+    G.Qw     = Qw;
+end
